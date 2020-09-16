32ad5bdf6bd77d7a3f688e43be3a3505a4860c3f
Dwayne <deandraper82@gmail.com>
2020-09-15 21:56:31 -0500

    Complete pseudoclassical refactor

--- src/dancer.js
+++ src/dancer.js
@@ -1,4 +1,4 @@
-// Creates and returns a new dancer object that can step
+// //Creates and returns a new dancer object that can step
 // var makeDancer = function(top, left, timeBetweenSteps) {
 
 //   var dancer = {};
@@ -13,10 +13,10 @@
 //   };
 //   dancer.step();
 
-// dancer.setPosition = function(top, left) {
+//   dancer.setPosition = function(top, left) {
 //   // Use css top and left properties to position our <span> tag
 //   // where it belongs on the page. See http://api.jquery.com/css/
-//   //
+
 //   var styleSettings = {
 //     top: top,
 //     left: left
@@ -31,26 +31,21 @@
 //   return dancer;
 // };
 
-var Dancer = function(top, left, timeBetweenSteps) {
-  // this.dancer = {};
+var makeDancer = function(top, left, timeBetweenSteps) {
+  this.top = top;
+  this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   this.$node = $('<span class="dancer"></span>');
-  // this.setPosition(top, left);
+  this.step();
 };
-Dancer.prototype.step = function() {
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
-  setTimeout(function() {
-    step.call(this);
-  }, this.timeBetweenSteps);
+makeDancer.prototype.step = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
-Dancer.prototype.setPosition = function(top, left) {
-  // Use css top and left properties to position our <span> tag
-  // where it belongs on the page. See http://api.jquery.com/css/
-  //
+
+makeDancer.prototype.setPosition = function() {
   var styleSettings = {
-    top: top,
-    left: left
+    top: this.top,
+    left: this.left
   };
   this.$node.css(styleSettings);
 };
