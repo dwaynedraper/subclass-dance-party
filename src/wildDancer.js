ef2fa833415ecfbe1a709b73f5f17aed2dc03f1d
Dwayne <deandraper82@gmail.com>
2020-09-16 22:43:39 -0500

    Implemented wildDancer functionality and boy is it wild!!!

--- src/wildDancer.js
+++ src/wildDancer.js
@@ -2,14 +2,23 @@ var makeWildDancer = function(top, left, timeBetweenSteps) {
   makeDancer.apply(this, arguments);
   this.$node = $('<span class="wildDancer"></span>');
   this.setPosition(this.top, this.left);
+  this.step();
 };
 makeWildDancer.prototype = Object.create(makeDancer.prototype);
 makeWildDancer.prototype.constructor = makeWildDancer;
 makeWildDancer.prototype.oldStep = makeDancer.prototype.step;
 makeWildDancer.prototype.step = function() {
   this.oldStep();
-  // this.top this.left
-};
+  //update timeBetweenSteps to a set value to make a smooth transition
+  // this.timeBetweenSteps = 16;
 
-//get initial position
-//iterate
+  this.top = this.top + 5;
+  if (this.top >= window.screen.height) {
+    this.top = 1;
+  }
+  this.left = this.left + 5;
+  if (this.left >= window.screen.width) {
+    this.left = 1;
+  }
+  this.setPosition(this.top, this.left);
+};
